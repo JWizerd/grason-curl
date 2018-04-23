e370d2c569b29fe8472ee72db64bbe91a9ba0616
Jeremiah Wodke <jeremiah@Jeremiahs-iMac.local>
2018-04-23 15:53:35 -0600

    commented code for better readability for future golivers

--- estatesalesnet/index.php
+++ estatesalesnet/index.php
@@ -1,12 +1,25 @@
 <?php 
 
+/**
+ * @author  Jeremiah Wodke <[<jeremiah.wodke@madwiremedia.com>]> 2018
+ */
+
 $creds = include '../api_credentials.php';
 require '../BaseApi.php';
 
 class Net extends BaseApi 
 {
 	const BASE_URL = 'https://www.estatesales.net';
+
+    /**
+     * the default sale type for 'EstateSale' as defined by .net
+     */
     const SALE_TYPE = 1;
+
+    /**
+     * show address type is required I have no idea what 
+     * the different types are as there is no documentation
+     */
     const SHOW_ADDRESS_TYPE = 1;
 
 	public function __construct(array $details, $token)  
@@ -21,20 +34,31 @@ class Net extends BaseApi
         $this->set_api_base();
 	}
 
+    /**
+     * use this when building all Guzzle requests EXCEPT @see SELF::generate_temporary_access_token()
+     */
     protected function set_api_base() 
     {
         $this->api_base  = $this::BASE_URL . '/api/';
-
-        print_r($this->api_base);
         $this->set_header('Authorization', 'Bearer ' . $this->generate_temporary_access_token());
         $this->set_header('X_XSRF', 'X_XSRF');
     }
 
+    /**
+     * for better usability of GuzzleHttp\Client instantiation
+     * @return [obj]
+     */
     protected function api() 
     {
         return (new GuzzleHttp\Client(['base_uri' => $this->api_base]));   
     }
 
+    /**
+     * All api requests require a temporary access_token that technically expires in 30 min
+     * but since this is GoLive and we don't have time to accommodate for the myriad of edge cases
+     * that an application such as this provides... We generate an access token every time a request is made
+     * @return [string] [temporary access token for all api requests]
+     */
 	protected function generate_temporary_access_token() 
     {
         $client = new GuzzleHttp\Client(['base_uri' => 'https://www.estatesales.net']);
@@ -53,6 +77,17 @@ class Net extends BaseApi
         return json_decode($response->getBody())->access_token;
 	}
 
+    /**
+     * Take a path to an image [in this case the post thumbnail url]
+     * convert into a byte array which in this case is a massive array 
+     * of ASCII chart Indexes
+     *
+     * Of course this is only one parameter that is required for an image 
+     * post request. The url to the image is also required LOL. 
+     * @param  [string] absolute image path
+     * @return [array] massive image byte array
+     * @see   [<description>]
+     */
     protected function convert_image_to_byte_array($image_path) 
     {
 
@@ -87,7 +122,12 @@ class Net extends BaseApi
 
     }
 
-    protected function create_sale() 
+    /**
+     * POST a sale to a .net account
+     * @return response obj containing a new listings id
+     * @todo  store listing id in Listing database
+     */
+    public function create() 
     {
         $response = $this->api()->request('POST', 'public-sales', [
             'headers' => $this->headers,
@@ -107,6 +147,21 @@ class Net extends BaseApi
 
         return json_decode($response->getBody());
     }
+
+    protected function post_images(array $image_paths) 
+    {
+
+    }
+
+    public function update($id) 
+    {
+
+    }
+
+    public function delete($id) 
+    {
+        
+    }
 }
 
 // testing
@@ -121,4 +176,4 @@ $details = [
 ];
 
 $net = new Net($details, $creds['net']['refresh_token']);
-print_r($net->create_sale());
+// print_r($net->create());
